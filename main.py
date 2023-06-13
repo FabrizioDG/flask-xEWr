16eead08bd6541a32c044169235d4eb6dc70b267
FabrizioDG <61915156+FabrizioDG@users.noreply.github.com>
2023-06-13 15:37:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def index():
     projection = {'_id': 1, 'suscriptions': 1, 'gender' : 1, 'degree' : 1, 'age' : 1, 'following' : 1,
                     'skills' : 1, 'hobbies' : 1, "userType" : 1, "username" : 1}
     df_users = pd.DataFrame(gr(collection.find({}, projection)))
-    return str(df_users.head().values)
+    return df_users.head(10).values
 
 
 if __name__ == '__main__':
