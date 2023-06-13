d709597ca6f75247bc0f8dc6a352c9568505b9ad
FabrizioDG <61915156+FabrizioDG@users.noreply.github.com>
2023-06-13 14:24:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from flask import Flask, jsonify
 import os
+import pymongo
+import pandas as pd 
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    def gr(result):
+        return [x for x in result]
+    
+    url = "mongodb+srv://dt_user:Edem_2023@cluster0.60xzxfx.mongodb.net"
+    client = pymongo.MongoClient(url) #(os.getenv('URL_MONGODB'))
+    db = client.app_dt
+
+    collection = db.users
+    projection = {'_id': 1, 'suscriptions': 1, 'gender' : 1, 'degree' : 1, 'age' : 1, 'following' : 1,
+                    'skills' : 1, 'hobbies' : 1, "userType" : 1, "username" : 1}
+    df_users = pd.DataFrame(gr(collection.find({}, projection)))
+    return df_users.to_json()
 
 
 if __name__ == '__main__':
